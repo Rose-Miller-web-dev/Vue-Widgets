a08efc18abe75a95671264a6840a7627b4e288b0
rose-miller-web-dev <rosemillerupworkfreelancer@gmail.com>
2023-09-20 15:00:47 -0500

    edrag and drop tested

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-
+  <TestComponent />
 </template>
 
 <script>
+import TestComponent from './components/TestComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    TestComponent,
   }
 }
 </script>
