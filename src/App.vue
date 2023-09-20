375352d65d12eccba5dd21f692e5f22030255556
rose-miller-web-dev <rosemillerupworkfreelancer@gmail.com>
2023-09-20 14:50:19 -0500

    drag and drop added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <TestComponent />
+
 </template>
 
 <script>
-import TestComponent from './components/TestComponent.vue'
+
 export default {
   name: 'App',
   components: {
-    TestComponent,
+    
   }
 }
 </script>
---
+++ src/components/DragDrop.vue
@@ -0,0 +1,32 @@
+<template>
+   <h1>My Anime List</h1>
+
+   <draggable v-model="animeList" tag="ul">
+    <template #item="{ element: anime }">
+        <li class="list-group-item">{{ anime }}</li>
+    </template>
+   </draggable>
+   
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  import draggable from 'vuedraggable'
+
+  const animeList = ref([
+    'One Punch Man',
+    'Attack On Titon',
+    'Parasite',
+    'My Hero Academy'
+  ])
+  
+  </script>
+  
+  <style>
+
+  li {
+    cursor: pointer;
+  }
+  
+  </style>
+  
